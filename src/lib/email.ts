6e5128153de39603bdbfd75ed8bea841a8942175
Knarkzel <knarkzel@gmail.com>
2025-01-04 01:52:20 +0100

    ci/cd

--- src/lib/email.ts
+++
@@ -1,5 +0,0 @@
-import { Resend } from "resend";
-import { env } from "$env/dynamic/private";
-
-// Create Resend client
-export const resend = env.RESEND_API_KEY ? new Resend(env.RESEND_API_KEY) : null;
--- src/lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from "stripe";
-import { env } from "$env/dynamic/private";
-
-// Create Stripe client
-export const stripe = env.STRIPE_SECRET_KEY
-  ? new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: "2024-12-18.acacia",
-  })
-  : null;
