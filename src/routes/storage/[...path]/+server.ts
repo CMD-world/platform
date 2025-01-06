5f6166a3e5429493cb1fbb6144cabd745cec3721
Knarkzel <knarkzel@gmail.com>
2025-01-06 12:18:11 +0100

    user-only folders

--- src/routes/storage/[...path]/+server.ts
+++ src/routes/storage/[...path]/+server.ts
@@ -1,7 +1,11 @@
 import { storage } from "$lib/storage";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async ({ params: { path } }) => {
-  const stream = await storage.readToBuffer(path);
-  return new Response(stream);
+export const GET: RequestHandler = async ({ params: { path }, locals: { user } }) => {
+  if (user && path.startsWith(`${user.id}/`)) {
+    const stream = await storage.readToBuffer(path);
+    return new Response(stream);
+  } else {
+    return new Response(null, { status: 403 });
+  }
 };
