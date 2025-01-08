f332765e719355cab477103394362cb0edef6d43
Knarkzel <knarkzel@gmail.com>
2025-01-08 12:10:19 +0100

    langfuse working

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -7,4 +7,11 @@ export const openai = observeOpenAI(
   new OpenAI({
     apiKey: env.OPENAI_API_KEY,
   }),
+  {
+    clientInitParams: {
+      baseUrl: env.LANGFUSE_HOST,
+      publicKey: env.LANGFUSE_PUBLIC_KEY,
+      secretKey: env.LANGFUSE_SECRET_KEY,
+    },
+  },
 );
