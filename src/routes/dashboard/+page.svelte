dbe2fc9d25f1dfc3c5d1718906baa2c034ccc606
Knarkzel <knarkzel@gmail.com>
2025-01-09 16:23:37 +0100

    nice frontend

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
   import PrivyLogout from "$components/PrivyLogout.svelte";
+
+  // State
+  let copied = $state(false);
+  let apiKey = $state<string | undefined>();
+
+  // API Key
+  async function generateApiKey() {
+    try {
+      const response = await fetch("/api/keys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          expiresAt: null,
+        }),
+      });
+      const data = await response.json();
+      apiKey = data.apiKey;
+    } catch (error) {
+      console.error("Error fetching keys:", error);
+    }
+  }
 </script>
 
 <svelte:head>
@@ -12,3 +35,33 @@
 </div>
 
 <p class="p mt-4">This is the dashboard page. It's where you can see your workflows, create new ones, and manage your account.</p>
+
+<button class="btn btn-primary mt-4" onclick={generateApiKey}>Generate API Key</button>
+
+{#if apiKey}
+  <h2 class="h2 mt-8">API Key</h2>
+  <p class="p mt-4">
+    Use this API Key to authenticate with the API by passing it as an Authorization header in your requests (Bearer token).
+  </p>
+  <div class="mt-4 rounded-lg bg-base-200 p-4">
+    <div class="flex items-center justify-between">
+      <p class="font-mono text-sm">{apiKey}</p>
+      <button
+        class="btn btn-ghost btn-sm"
+        onclick={() => {
+          if (apiKey) {
+            navigator.clipboard.writeText(apiKey);
+            copied = true;
+            setTimeout(() => (copied = false), 2000);
+          }
+        }}
+      >
+        {#if copied}
+          Copied! <span class="i-lucide-check"></span>
+        {:else}
+          Copy
+        {/if}
+      </button>
+    </div>
+  </div>
+{/if}
